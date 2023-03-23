07711c90920d2a5281cb7eb09b581a438f7a4a61
althrun <althrunsun@gmail.com>
2023-03-22 17:13:02 -0700

    EDA

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
-from dash import html, dcc
+from dash import html, dcc, Input, Output
 import altair as alt
 from vega_datasets import data
-
+import dash_bootstrap_components as dbc
+import dash
 
 # Read in global data
 cars = data.cars()
 
 # Setup app and layout/frontend
-app = dash.Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
-app.layout = html.Div([
-    html.Iframe(
-        id='scatter',
-        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-    html.Div(id='mean-x-div'),
-    html.Br(),
-    html.Label([
-        'X column',
-        dcc.Dropdown(
-            id='xcol-widget',
-            value='Horsepower',  # REQUIRED to show the plot on the first page load
-            options=[{'label': col, 'value': col} for col in cars.columns])]),
-    html.Label([
-        'Y column',
-        dcc.Dropdown(
-            id='ycol-widget',
-            value='Displacement',  # REQUIRED to show the plot on the first page load
-            options=[{'label': col, 'value': col} for col in cars.columns])])])
-
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = dbc.Container([
+    html.H1('My mudplank'),
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(
+                id='xcol-widget',
+                value='Horsepower',  # REQUIRED to show the plot on the first page load
+                options=[{'label': col, 'value': col} for col in cars.columns]),
+            dcc.Dropdown(
+                id='ycol-widget',
+                value='Displacement',  # REQUIRED to show the plot on the first page load
+                options=[{'label': col, 'value': col} for col in cars.columns])],
+            md=4),
+        dbc.Col(
+            html.Iframe(
+                id='scatter',
+                style={'border-width': '0', 'width': '100%', 'height': '400px'}))])])
 # Set up callbacks/backend
 @app.callback(
     Output('scatter', 'srcDoc'),
-    Output('mean-x-div', 'children'),
     Input('xcol-widget', 'value'),
     Input('ycol-widget', 'value'))
 def plot_altair(xcol, ycol):
@@ -38,7 +36,7 @@ def plot_altair(xcol, ycol):
         x=xcol,
         y=ycol,
         tooltip='Horsepower').interactive()
-    return chart.to_html(), f'The mean of {xcol} is {cars[xcol].mean().round(1)}'
+    return chart.to_html()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
